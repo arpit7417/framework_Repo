0fdfeace4a6a8751cb9aac946dd58b2207cc985a
arpit7417 <singhatul2833@gmail.com>
2026-03-20 13:01:34 +0530

    added new Test

--- src/test/java/pac1/ContactTest.java
+++ src/test/java/pac1/ContactTest.java
@@ -25,5 +25,10 @@ public class ContactTest {
 	public void modifycontactTest() {
 		System.out.println("execute modifycontactTest");
 	}
+	
+	@Test
+	public void deletecontactTest() {
+		System.out.println("execute deletecontactTest");
+	}
 
 }
